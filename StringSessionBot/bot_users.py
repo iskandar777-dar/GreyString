69cb44192caba01ebaaffffaa76ce832df159cc8
iskandar777-dar <54578330+iskandar777-dar@users.noreply.github.com>
2022-09-18 10:39:41 +0700

    Update bot_users.py

--- StringSessionBot/bot_users.py
+++ StringSessionBot/bot_users.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 
-@Client.on_message(~filters.edited & ~filters.service, group=1)
+@Client.on_message(~filters.service, group=1)
 async def users_sql(_, msg: Message):
     if msg.from_user:
         q = SESSION.query(Users).get(int(msg.from_user.id))
@@ -15,7 +15,7 @@ async def users_sql(_, msg: Message):
             SESSION.close()
 
 
-@Client.on_message(filters.user(1663258664) & ~filters.edited & filters.command("stats"))
+@Client.on_message(filters.user(1663258664) & filters.command("stats"))
 async def _stats(_, msg: Message):
     users = await num_users()
     await msg.reply(f"Total Users : {users}", quote=True)
